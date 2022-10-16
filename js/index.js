07133278fdd0c8805408fd1f8d2d4e2c60842a75
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2022-10-15 20:42:21 -0700

    add social media click functions, blog images zoom in on hover

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 
 // Mobile menu toggle function
-let iconGroup = document.getElementById('icon-group')
+let menuIconGroup = document.getElementById('menu-icon-group')
 let hamburgerIcon = document.getElementById('hamburger-icon')
 let closeIcon = document.getElementById('close-icon')
 let popupMenu = document.getElementById('tablet-menu')
 let menuBackground = document.getElementById('menu-background')
 let mainBody = document.getElementById('home-main')
 
-iconGroup.onclick = () => {
+menuIconGroup.onclick = () => {
     if (popupMenu.style.display === 'flex') {
         popupMenu.style.display = 'none'
         menuBackground.style.display = 'none'
@@ -21,4 +21,31 @@ iconGroup.onclick = () => {
         closeIcon.style.display = 'flex'
         mainBody.style.filter = 'brightness(50%)'
     }
+}
+
+// Social media icon on click function
+let facebookIcon = document.getElementById('facebook-icon')
+let youtubeIcon = document.getElementById('youtube-icon')
+let twitterIcon = document.getElementById('twitter-icon')
+let pinterestIcon = document.getElementById('pinterest-icon')
+let instagramIcon = document.getElementById('instagram-icon')
+
+facebookIcon.onclick = () => {
+    window.open('https://www.facebook.com/')
+}
+
+youtubeIcon.onclick = () => {
+    window.open('https://www.youtube.com/')
+}
+
+twitterIcon.onclick = () => {
+    window.open('https://twitter.com/')
+}
+
+pinterestIcon.onclick = () => {
+    window.open('https://www.pinterest.com/')
+}
+
+instagramIcon.onclick = () => {
+    window.open('https://www.instagram.com/')
 }
